a01353401d468120c72682a9fe38634fc2ebe1de
Joel <joel3000@gmail.com>
2020-03-01 18:17:44 -0800

    Almost works. Just need to fix affected services.

--- email_parser.py
+++ email_parser.py
@@ -18,9 +18,38 @@ PARSE_TRIGGERS = {'subject': ParseTypes.SINGLE,
                   'pw reference number': ParseTypes.SINGLE,
                   'maintenance window': ParseTypes.MULTI,
                   'location of work': ParseTypes.SINGLE,
-                  'affected services': ParseTypes.MULTI,
-                  'email': ParseTypes.SINGLE,
+                  'affected service(s)': ParseTypes.MULTI,
+                  'e-mail': ParseTypes.SINGLE,
                   'phone': ParseTypes.SINGLE}
+
+def single_line_parser(line: str) -> List:
+    """Return a two element list if the input of the line is
+        <key>: <value>
+
+        Returns a single element list if the input of the line is
+        <key>:
+        """
+    if line and line[0] in ["(","-","_"," "]:
+        return []
+    o = line.split(":", 1)
+    if len(o) == 2 and o[1] == '':
+        return [o[0]]
+    return o
+
+
+def next_line_idx(i: int, lines: List) -> int:
+    """Return the index of the next non-empty line."""
+    i += 1
+    while not lines[i] and i < len(lines):
+        i += 1
+    return i
+
+def is_pair(l: List) -> bool:
+    return len(l) == 2
+
+def is_single(l: List) -> bool:
+    return len(l) == 1 and l[0] != ''
+
 
 out = dict()
 
@@ -32,16 +61,40 @@ def email_parser(body: str) -> dict:
         line = lines[i]
         trigger = re.match(r'^([^:]+):', line.lower())
         if trigger and trigger[1] in PARSE_TRIGGERS.keys():
-           if PARSE_TRIGGERS[trigger] is ParseTypes.SINGLE:
-               # Format is <key>:<value>
-               # or
-               # <key>:
-               # <blank_line>[0,1]
-               # <value> - Assuming only a single line for now.
-
-
+           trg = trigger[1]
+           if PARSE_TRIGGERS[trg] is ParseTypes.SINGLE:
+                # Format is <key>:<value>
+                # or
+                # <key>:
+                # <blank_line>[0,1]
+                # <value> - Assuming only a single line for now.
+                slp = single_line_parser(line)
+                if is_pair(slp):
+                    # <k>:<v>
+                    out[trg] = slp[1]
+                elif is_single(slp):
+                    # <k>: \n\n?<v>\n
+                    i += 1
+                    while not lines[i]:
+                        i += 1
+                    out[trg] = lines[i]
+           if PARSE_TRIGGERS[trg] is ParseTypes.MULTI:
+                # Assume sub values are either
+                # single line <k>:<v>
+                # or a comment
+                # The multi-block ends with a new line
+                out[trg] = dict()
+                i += 1
+                while not lines[i]:
+                    i += 1 # advance to data
+                while lines[i]:
+                    slp = single_line_parser(lines[i])
+                    if is_pair(slp):
+                        out[trg][slp[0]] = slp[1]
+                    i += 1
 
         i += 1
+    return out
 
 
 if __name__ == '__main__':
